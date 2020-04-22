f850de35a962106e38750cdc193b0c3193beceaf
Brian Ketelsen <mail@bjk.fyi>
2020-04-22 14:50:19 -0400

    redirect

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  if (event.request.method == 'POST') {
+    event.respondWith(handlePost(event.request))
+  } else {
+    event.respondWith(handleRequest(event.request))
+  }
 })
+
+async function handlePost(request) {
+  const json = await request.json();
+
+/*
+    return new Response(JSON.stringify(json), {
+      status: Number(201)
+    });
+*/
+  if (json.model === 'redirect') {
+    await SHORT_KV.put(json.entry.short, json.entry.to)
+
+    return new Response('created' + JSON.stringify(json), {
+      status: Number(201)
+    });
+
+  }
+  return new Response('Failed', {
+    status: Number(400)
+  });
+
+}
 
 async function handleRequest(request) {
   const url = new URL(request.url);
@@ -10,15 +36,15 @@ async function handleRequest(request) {
     return new Response('', {
       status: Number(301),
       headers: {
-          'Location': 'https://brianketelsen.com/pages/not-found?slug=' + uri
+        'Location': 'https://brianketelsen.com/pages/not-found?slug=' + uri
       }
-  });
+    });
   }
 
   return new Response('', {
     status: Number(301),
     headers: {
-        'Location': value
+      'Location': value
     }
-});
+  });
 }

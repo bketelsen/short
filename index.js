392dd0afabd56d6af8c9a8c68383256fe0e6445a
Brian Ketelsen <mail@bjk.fyi>
2020-04-22 15:41:15 -0400

    added content tracking

--- index.js
+++ index.js
@@ -9,14 +9,24 @@ addEventListener('fetch', event => {
 async function handlePost(request) {
   const json = await request.json();
 
-/*
-    return new Response(JSON.stringify(json), {
-      status: Number(201)
-    });
-*/
+  /*
+      return new Response(JSON.stringify(json), {
+        status: Number(201)
+      });
+  */
   if (json.model === 'redirect') {
     await SHORT_KV.put(json.entry.short, json.entry.to)
 
+    if (json.entry.hasOwnProperty('alias')) {
+      await SHORT_KV.put(json.entry.short + '-alias', json.entry.alias);
+    }
+
+    if (json.entry.hasOwnProperty('code')) {
+      await SHORT_KV.put(json.entry.short + '-code', json.entry.code);
+    }
+    if (json.entry.hasOwnProperty('source')) {
+      await SHORT_KV.put(json.entry.short + '-source', json.entry.source);
+    }
     return new Response('created' + JSON.stringify(json), {
       status: Number(201)
     });
@@ -41,10 +51,46 @@ async function handleRequest(request) {
     });
   }
 
+  const code = await SHORT_KV.get(uri + '-code');
+  let extra = ''
+  if (code === null) {
+    return new Response('', {
+      status: Number(301),
+      headers: {
+        'Location': value 
+      }
+    });
+  }
+  // we have a code
+  extra = '?wt.mc_id='
+  extra = extra + code
+
+  const source = await SHORT_KV.get(uri + '-source');
+  if (source === null) {
+    return new Response('', {
+      status: Number(301),
+      headers: {
+        'Location': value 
+      }
+    });
+  }
+    // we have a source
+    extra = extra + '-' + source
+  const alias = await SHORT_KV.get(uri + '-alias');
+  if (alias === null) {
+    return new Response('', {
+      status: Number(301),
+      headers: {
+        'Location': value 
+      }
+    });
+  }
+    // we have an alias
+    extra = extra + '-' + alias
   return new Response('', {
     status: Number(301),
     headers: {
-      'Location': value
+      'Location': value + extra
     }
   });
 }
